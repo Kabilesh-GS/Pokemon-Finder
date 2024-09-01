93c4f269e97e4b14c410a68211b8bcfb4b18410e
Kabilesh-GS <kabileshgs@gmail.com>
2024-09-01 10:21:17 +0530

    Base app done

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,8 @@
 let input = document.getElementById('input');
 let submit = document.getElementById('submit');
 let image = document.getElementById('image');
+let nameCont = document.getElementById('name');
+let weight = document.getElementById('weight');
 
 submit.addEventListener('click', () => {
   let pokemon = input.value;
@@ -16,11 +18,14 @@ submit.addEventListener('click', () => {
       const data = await response.json();
       console.log(data); 
 
+      let PokeWeight = data.weight;
       let PokeName = data.name;
       let PokeImage = data.sprites.front_default
 
+      nameCont.innerHTML = PokeName;
       image.src = PokeImage;
       image.style.display = 'block';
+      weight.innerHTML = `Weight : ${PokeWeight}`;
     }
     catch(error){
       console.error(error);
